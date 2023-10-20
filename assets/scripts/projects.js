caa155ee322e0b1bf3df42a7f5c05cfcfb3642f9
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-10-20 21:47:29 +0100

    added screenshots to projects

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -14,9 +14,18 @@ async function show_project_cards() {
         card.className = 'card'
         const card_cover = document.createElement('img')
         card_cover.src = project.card
+        card_cover.addEventListener('click',() => {
+            open_project(project)
+        })
         card.appendChild(card_cover)
         projects_section.appendChild(card)
     })
+}
+
+function open_project(project) {
+    console.log(project)
+    // const screenshot = document.createElement('img')
+    // screenshot.src = `${project.screenshots}/1.png`
 }
 
 const projects_section = document.querySelector('#projects')
