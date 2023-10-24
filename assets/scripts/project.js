b73d368887cbdf1f9190842bac466c3b5d059f36
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-10-24 17:12:55 +0100

    made project title into link

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -10,9 +10,14 @@ async function show_project() {
     console.log(projects)
     console.log(project)
 
-    const title = document.createElement('p')
+    const title_container = document.createElement('p')
+    title_container.className = 'project-title'
+    const title = document.createElement('a')
     title.className = 'project-title'
+    title.href = project.link
+    title.target = "_blank"
     title.textContent = (project.name).toUpperCase()
+    title_container.appendChild(title)
 
     const project_role = document.createElement('p')
     project_role.className = 'role'
@@ -94,7 +99,7 @@ async function show_project() {
         links_section.append(server_link)
     }
 
-    project_section.append(title, project_role, ss_section, desc_section, links_section)
+    project_section.append(title_container, project_role, ss_section, desc_section, links_section)
 
     const left_project_btn = document.createElement('span')    
     left_project_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z"/></svg>'
